b1228a7324ed3f01b1d97d031ad78629e89d1ea7
Shenkang Zhai <zhshkang0@outlook.com>
2025-10-08 02:03:09 +0800

    Add DashScope model configuration

---
+++ config.py
@@ -0,0 +1,4 @@
+DASHSCOPE_ENDPOINT = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+DASHSCOPE_MODEL = "qwen2.5-vl"
+DASHSCOPE_API_KEY = "sk-e0a520f500c64b3199492e958545fffc"
+DASHSCOPE_PROVIDER = "dashscope"
